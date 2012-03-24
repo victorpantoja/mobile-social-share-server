0ab5d9e7cf9cad6c4d6199b1899aef8fce88d1c4
Victor Pantoja <victor.pantoja@gmail.com>
2012-03-24 15:33:50 -0300

    refactoring status

--- mss/handler/status.py
+++ mss/handler/status.py
@@ -3,23 +3,7 @@
 
 from mss.handler.base import BaseHandler
 
-class StatusHandler(BaseHandler):
-    """
-        Controller de Status do Servidor
-    """
-       
-    def get(self, **kw):
-        """
-        <h2><b>Obter o status do sistema</b></h2><br>
-        Serviço que retorna uma página com uma mensagem aos usuários e servir de canal de comunicação entre o administraor do sistema e o usuário.<br>
-        <br><h3><b>Parâmetros:</b></h3><br>
-        auth: string de autenticação do usuário no MSS <br />
-        <br><h3><b>Retorno:</b></h3><br>
-        HTML com a informação cadastrada pelo adminstrador.
-        """
-        
-        self.post(**kw)
 
-    def post(self, **kw):
-        
-        self.render_template("status.html")
+class StatusHandler(BaseHandler):
+    def status(self, **kw):
+        return self.render_to_template("/status.html")
